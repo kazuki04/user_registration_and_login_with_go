9215996243d59689fc2ea83b20f0f7bec8edf772
kazuki04 <kazukik0402@gmail.com>
2021-09-25 19:18:41 +0900

    modify config

--- config/config.go
+++ config/config.go
@@ -13,7 +13,7 @@ type ConfigList struct {
 var Config ConfigList
 
 func init() {
-	cfg, err := ini.Load("../config.ini")
+	cfg, err := ini.Load("./config.ini")
 	if err != nil {
 		fmt.Printf("Fail to read file: %v", err)
 		os.Exit(1)
