6a0a8dfa6827c9d9fb9928f852bd128dd5216f65
kazuki04 <kazukik0402@gmail.com>
2021-09-25 18:13:22 +0900

    set up config

---
+++ utils/config.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"fmt"
+	"gopkg.in/ini.v1"
+	"os"
+)
+
+type ConfigList struct {
+	Port int
+}
+
+var Config ConfigList
+
+func init() {
+	cfg, err := ini.Load("../config.ini")
+	if err != nil {
+		fmt.Printf("Fail to read file: %v", err)
+		os.Exit(1)
+	}
+	Config = ConfigList{
+		Port: cfg.Section("web").Key("port").MustInt(),
+	}
+}
