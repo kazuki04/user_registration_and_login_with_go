0312114983a16b3e8ab4f674aec96888c8329194
kazuki04 <kazukik0402@gmail.com>
2021-09-26 17:14:26 +0900

    add password field to User struct

--- app/models/user.go
+++ app/models/user.go
@@ -8,6 +8,7 @@ type User struct {
 	ID        uint   `gorm:"primaryKey"`
 	Name      string `json:"name"`
 	Email     string `json:"email"`
+	Password  string `json:password`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
